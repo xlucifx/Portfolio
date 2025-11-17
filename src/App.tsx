2d3972e88dc932c09286fd5ffd043e97b6340d0b
Jakub Guzy <s30086@pjwstk.edu.pl>
2025-11-17 13:44:46 +0100

    Fix GitHub Pages base path

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
 import SquareContent from "./components/Square";
